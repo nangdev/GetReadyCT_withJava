d4d1f003401f5242892e090f8459bd903c96987b
전승현 <zovl5546@naver.com>
2024-09-27 17:59:56 +0900

    9/27 study

--- src/main/java/study/binarysearch/P2470.java
+++ src/main/java/study/binarysearch/P2470.java
@@ -21,13 +21,40 @@ public class P2470 {
         }
 
         Arrays.sort(arr);
-
-        long min = 0;
-        long max = arr[n - 1] + 1;
-
-        while (min < max) {
-            long mid = (min + max) / 2;
-
-        }
+ 
+        long num1 = arr[0];
+        long num2 = arr[n-1];
+        
+        for (int i = 0; i < n; i++) {
+			long temp1 = arr[i];
+			long temp2 = 0;
+			
+			int min = 0;
+			int max = n;
+			
+			long add = arr[n-2]+num2;
+			
+			while (min < max) {
+				int mid = (min+max)/2;
+				
+				long val = Math.abs(arr[mid]+temp1);
+				
+				if(val < add) {
+					min = mid+1;
+					add = val;
+				}else{
+					max = mid;
+				}
+			}
+			temp2 = arr[min-1];
+        	
+        	if(Math.abs(num1+num2) > Math.abs(temp1+temp2)) {
+        		num1 = temp1;
+        		num2 = temp2;
+        	}
+		}
+        
+        System.out.println(num1 +" "+num2);
+       
     }
 }
