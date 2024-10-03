077cf20d05344ece2748ecb709458ffddf76ab49
nangdi <zovl5546@naver.com>
2024-10-04 00:15:26 +0900

    10/03 study

---
+++ src/main/java/progammers_Lv2/P72411.java
@@ -0,0 +1,7 @@
+package progammers_Lv2;
+
+public class P72411 {
+    public static void main(String[] args) {
+    
+    }
+}
--- src/main/java/study/binarysearch/P1300.java
+++ src/main/java/study/binarysearch/P1300.java
@@ -3,11 +3,30 @@ package study.binarysearch;
 import java.util.Scanner;
 
 public class P1300 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int k = sc.nextInt();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
-	}
+        long min = 1;
+        long max = k;
+
+        while (min < max) {
+            long mid = (min + max) / 2;
+
+            long cnt = 0;
+            for (int i = 1; i <= n; i++) {
+                cnt += Math.min(n, mid / i);
+            }
+
+            if (cnt < k) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+
+        System.out.println(min);
+    }
 }
