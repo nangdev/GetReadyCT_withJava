1b05ad60ceb6d995f5619f78c6709e8508aedd56
전승현 <zovl5546@naver.com>
2024-10-11 17:28:32 +0900

    10/11 study

---
+++ src/main/java/study/problem/P23971.java
@@ -0,0 +1,21 @@
+package study.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P23971 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int h = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+	}
+
+}
