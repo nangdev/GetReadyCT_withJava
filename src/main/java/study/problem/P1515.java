468d62df4ea028649dff588f333a5077130d45ef
HomeCpu <zovl5546@naver.com>
2024-11-05 23:49:56 +0900

    11/5 study

---
+++ src/main/java/study/problem/P1515.java
@@ -0,0 +1,12 @@
+package study.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1515 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+    }
+}
